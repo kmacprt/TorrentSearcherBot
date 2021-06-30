cd1b079b764ac9a4107ccb4b6f9a3d7f44b6ecaf
KAJ <72274248+kmacprt@users.noreply.github.com>
2021-06-29 23:22:25 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-TOKEN = os.environ.get("BOT_TOKEN")
-API_HASH = os.environ.get("API_HASH")
-API_ID = int(os.environ.get("API_ID"))
-START_MESSAGE = os.environ.get("START_MESSAGE", "<b>Hi ! I am a simple torrent searcher using @sjprojects's Torrent Searcher api.\n\n\nMade with 🐍 by @KeralasBots</b>")
-FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "<b>Made with ❤️ by @KeralasBots</b>")
+TOKEN = "1630350451:AAE00WTEIjAr7vhIGOue43rH8dvIeCuD46Q"
+API_HASH = "b994bccad6118cd6fb6d091b9bbfe5cf"
+API_ID = 1971546
+START_MESSAGE = "START_MESSAGE", "<b>Hi ! I am a simple torrent searcher using @sjprojects's Torrent Searcher api.\n\n\nMade with 🐍 by @KeralasBots</b>"
+FOOTER_TEXT = "FOOTER_TEXT", "<b>Made with ❤️ by @KeralasBots</b>"
 TORRENTS = {}
